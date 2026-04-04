8dc615bf715dce710ac04feb817c8a5443d92a4d
qe201020335 <qe201020335@sina.com>
2026-04-04 01:42:01 -0400

    Test runner

---
+++ BSBisecter/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+
+
+Console.WriteLine(Run());
+
+
+bool Run()
+{
+    var psi = new ProcessStartInfo("BSTester")
+    {
+        UseShellExecute = false,
+        RedirectStandardOutput = true
+    };
+    using var process = Process.Start(psi) ?? throw new InvalidOperationException("Failed to start process");
+    var output = "";
+    process.OutputDataReceived += (_, args) =>
+    {
+        Console.WriteLine(args.Data);
+        output += args.Data + "\n";
+    };
+    process.BeginOutputReadLine();
+    process.WaitForExit();
+    return output.Contains("TRAMPOLINE ERROR DETECTED");
+}
